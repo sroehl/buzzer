3d2cf850bc24cf42fa067d9ce1409ff7153e0f53
steve <steve@steve-VirtualBox.(none)>
2013-09-13 15:34:57 -0500

    Made many improvements, including but not limited to including an actual buzz, users can join rooms correctly, and you can clear the screen

--- app.js
+++ app.js
@@ -1,32 +1,67 @@
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
-  , fs = require('fs')
+  , fs = require('fs');
+
+var url = require('url');
 
 app.listen(8999);
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
+  var path = url.parse(req.url).pathname;
+  var fsCallback = function (err, data) {
+    if (err) throw error;
       res.writeHead(500);
-      return res.end('Error loading index.html');
+      res.write(data);
+      res.end();
     }
-   console.log(__dirname);
 
-    res.writeHead(200);
-    res.end(data);
-  });
+   console.log(path);
+  switch(path){
+    case '/teacher':
+      doc=fs.readFile(__dirname + '/teacher.html',fsCallback);
+      break;
+   default:
+     doc=fs.readFile(__dirname + '/student.html',fsCallback);
+    break;
+  }
 }
 
+function getUsers2(room){
+for (var socketId in io.sockets.clients(room)) {
+    io.sockets.sockets[socketId].get('nickname', function(err, nickname) {
+        console.log(nickname);
+    });
+}
+}
+
+function getUsers(room){
+var clients = io.sockets.clients(room);
+console.log(clients[0]);
+for (var client  in clients) {
+   console.log(client);
+
+}
+}
 
 io.sockets.on('connection', function (socket) {
   var room;
-  socket.on('draw', function(x,y)  {
-    console.log(x,y);
-    io.sockets.in(room).emit('sdraw',x,y);
+  socket.on('buzz', function()  {
+    console.log("buzzed");
+    var nick;
+    io.sockets.sockets[socket.id].get('nickname', function(err, nickname) {
+        nick=nickname;
+        console.log(nick);
+    });
+    io.sockets.in(room).emit('buzzed',nick);
   });
 
-socket.on('subscribe', function(data) { socket.join(data); room=data;})
+socket.on('subscribe', function(roomId,user) { 
+       socket.leave(room);
+       socket.join(roomId); 
+       room=roomId;
+       socket.set('nickname',user);
+      console.log('joined');
+ });
 
 });
 
