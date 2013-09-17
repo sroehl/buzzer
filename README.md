92496ce392d3927b440693b958dde3a6202fe8d3
Steve Roehl <stevenroehl@gmail.com>
2013-09-16 23:46:09 -0500

    Made login screen look pretty.  Also implemented functions so that when the user leaves in anyway they get removed from the room.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-buzzer
+Buzzer
 ======
 
 A simple buzzer program for teachers
+
+
+It uses node.js and socket.io to provide real time buzzer program that is created in such a way that a web browser on any computer (MAC,PC,Android,iOS) can be used together and with minimum effort for the user.
--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var app = require('http').createServer(handler)
 var url = require('url');
 
 var rooms = new Array();
+var buzzed = 0;
 
 app.listen(8999);
 
@@ -32,6 +33,8 @@ function handler (req, res) {
 io.sockets.on('connection', function (socket) {
   var room;
   socket.on('buzz', function()  {
+   if(!(buzzed)){
+    buzzed=1;
     console.log("buzzed");
     var nick;
     io.sockets.sockets[socket.id].get('nickname', function(err, nickname) {
@@ -39,10 +42,15 @@ io.sockets.on('connection', function (socket) {
         console.log(nick);
     });
     io.sockets.in(room).emit('buzzed',nick);
+   }
   });
 
 socket.on('subscribe', function(roomId,user) { 
-      socket.leave(room);
+       if(typeof rooms[room] != 'undefined'){
+         console.log(typeof rooms[room]);
+         removeUser();
+       }
+       socket.leave(room);
        socket.join(roomId); 
        room=roomId;
        socket.set('nickname',user);
@@ -54,12 +62,33 @@ socket.on('subscribe', function(roomId,user) {
       }else{
         rooms[room].push(user);
       }
+    io.sockets.in(room).emit('users',rooms[room]);
  });
+
+socket.on('disconnect', function (){
+  if(typeof rooms[room] != 'undefined'){
+    removeUser();
+  }
+});
+
+
+socket.on('getUser',function(){
+  io.sockets.in(room).emit('users',rooms[room]);
+});
 
 
   socket.on('clear', function()  {
+    buzzed=0;
     io.sockets.in(room).emit('clearCanvas');
   });
+
+function removeUser(){
+  io.sockets.sockets[socket.id].get('nickname', function(err,user){
+    var index=rooms[room].indexOf(user);
+    rooms[room].splice(index,1);
+  });
+  io.sockets.in(room).emit('users',rooms[room]);
+}
 
 
 });
